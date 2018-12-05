7252bb4044bdef1192511628e60b069401cdd414
marimeli <marimel.casas@gmail.com>
2018-12-05 06:56:41 -0500

    Updates

---
+++ precache-manifest.806bd3124337aa385ed29dab04eeff04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "850c2c1f8e3bc78db744",
+    "url": "/weather-app/static/js/main.850c2c1f.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/weather-app/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "850c2c1f8e3bc78db744",
+    "url": "/weather-app/static/css/main.d69cf9c3.chunk.css"
+  },
+  {
+    "revision": "e6f0c33bf5ae62329848bbac83e650d5",
+    "url": "/weather-app/index.html"
+  }
+];
